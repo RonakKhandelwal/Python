48b64881c68023b14a121c0c60e64a01e3952220
CryNoz <ronak.khandelwal@learner.manipal.edu>
2015-12-30 09:11:54 +0530

    Some More FIles

---
+++ UG2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+
+import sys
+
+int_condition =06
+
+if int_condition < 6:
+	print int_condition
+	sys.exit("We had some problelm")
+
+else :
+	sys.exit("No problem sir do whatever the fuck you want")	
---
+++ UG3.py
@@ -0,0 +1,15 @@
+import sys
+
+
+
+some_number=raw_input("Enter an integer")
+
+try:
+	some_int=int(some_number)
+except ValueError:
+	sys.exit("You Should've entered an int bitch")
+
+
+print some_int
+
+print "Sometime I write wy too fast for anyone to understand clearly what I am  actually typing and I'm getting goood at this stuff "
